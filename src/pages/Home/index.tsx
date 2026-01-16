0780b5d3d2ca9dd0a1644d83c4febc138cf7a4ff
Eduardo Schubert <duduschubert@email.com>
2026-01-16 00:43:27 -0300

    Modificações 16/01/2026

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,11 @@
 import { Container } from '../../components/Container';
 import { CountDown } from '../../components/CountDown/Index';
 import { MainForm } from '../../components/MainForm';
-import { MainTemplate } from '../../Templates/MainTemplate';
+import { MainTemplate } from '../../Templates/MainTemplate'; 
+
+
+// Essa parte será o miolo da página que irá trocar usando sempre a base MainTemplate
+
 
 export function Home() {
   return (
